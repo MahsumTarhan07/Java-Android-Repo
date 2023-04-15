b30f31a5f1a68a79482c6652674fc99dc7d75a91
MahsumTarhan07 <mahsumtarhan1@gmail.com>
2023-04-15 23:53:14 +0300

    Text Button Edit Text

---
+++ ButtonTextViewEditTextKulanimi/app/src/main/java/com/example/buttontextviewedittext/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.buttontextviewedittext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             String alinanVeri = editText.getText().toString();
+             textView.setText(alinanVeri);
+            }
+        });
+
+    }
+}
